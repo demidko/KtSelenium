064891ab68d7abdaee0457370703b49aefae056a
Daniel Demidko <demidko@farpost.com>
2020-11-12 15:29:56 +1000

    Добавил Selenium

--- src/main/kotlin/cf/demidko/Application.kt
+++ src/main/kotlin/cf/demidko/Application.kt
@@ -1,5 +1,18 @@
 package cf.demidko
 
+import io.github.bonigarcia.wdm.WebDriverManager
+import io.github.bonigarcia.wdm.WebDriverManager.firefoxdriver
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import java.io.BufferedInputStream
+import java.io.Closeable
+import java.io.InputStream
+import java.lang.Thread.sleep
+
 fun main() {
-  println("It works")
+  firefoxdriver().setup()
+  val driver = FirefoxDriver()
+  driver.get("https://google.com")
+  Thread.sleep(1000 * 3)
+  driver.close()
 }
